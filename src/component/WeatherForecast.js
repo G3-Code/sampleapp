dc205176587c4015115fc752ab15113cffd332eb
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-28 09:12:44 -0600

    WeatherForecast -> Added developer comments

--- src/component/WeatherForecast.js
+++ src/component/WeatherForecast.js
@@ -3,26 +3,32 @@ import Weather from "./Weather";
 import CityHeader from "./CityHeader";
 import CurrentWeather from "./CurrentWeather";
 
-class WeatherForecast extends React.Component {
-  render() {
-    let { city, cities, currentForecast, forecasts } = this.props.forecastInfo;
-    return (
-      <div className="weather-forecast">
-        <CityHeader
-          city={city}
-          cities={cities}
-          handleSelect={this.props.handleSelect}
-        />
+/**
+ * Functional component which uses three reusable components to display the
+ * weather forecast
+ *
+ * @param {Object} props
+ */
+export default function WeatherForecast(props) {
+  // Object destructuring of forecastInfo
+  let { city, cities, currentForecast, forecasts } = props.forecastInfo;
+  return (
+    <div className="weather-forecast">
+      {/* CITY HEADER */}
+      <CityHeader
+        city={city}
+        cities={cities}
+        handleSelect={props.handleSelect}
+      />
+      {/* TODAY'S WEATHER FOR THE CITY */}
+      <CurrentWeather currentForecast={currentForecast} />
 
-        <CurrentWeather currentForecast={currentForecast} />
-        <div className="forecasts">
-          {forecasts.map((forecast, index) => (
-            <Weather key={index} forecast={forecast} />
-          ))}
-        </div>
+      {/* WEATHER CARDS BASED ON THE NUMBER OF FORECAST AVAIALBLE FROM DB */}
+      <div className="forecasts">
+        {forecasts.map((forecast, index) => (
+          <Weather key={index} forecast={forecast} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default WeatherForecast;
