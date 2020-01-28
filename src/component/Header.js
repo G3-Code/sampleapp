04beaae86b018c8e9cfa39635be5473737c8defb
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-28 10:08:24 -0600

    Header -> Developer comments

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Logo from "../assets/weather-2-icon.png";
 
+/**
+ * Header containing the logo and title
+ *
+ * @param {Object} props
+ */
 export default function Header(props) {
   return (
     <nav>
