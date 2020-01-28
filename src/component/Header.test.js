a7476bc6c57612f1687348d897e7210328109e87
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-28 13:30:56 -0600

    Starting to add tests for react-redux. Started with header

---
+++ src/component/Header.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./Header";
+import { findByTestAttr } from "../utils";
+
+const setUpTest = (props = {}) => {
+  const component = shallow(<Header {...props} />);
+  return component;
+};
+
+describe("TS1: Testing the header component", () => {
+  let component;
+  beforeEach(() => {
+    const props = {
+      name: "John"
+    };
+    component = setUpTest(props);
+  });
+  it("TC1: Should render without errors", () => {
+    const wrapper = findByTestAttr(component, ".img-weather");
+    expect(wrapper.length).toBe(1);
+  });
+
+  it("TC2: Should render a logo", () => {
+    const logo = findByTestAttr(component, ".logo-header");
+    expect(logo.length).toBe(1);
+  });
+
+  it("TC3: Check if image exist", () => {
+    const logo = findByTestAttr(component, "img");
+    expect(logo.length).toBe(1);
+  });
+
+  it("TC4: Check if header exist", () => {
+    const logo = findByTestAttr(component, "h1");
+    expect(logo.length).toBe(1);
+  });
+});
