4f41676078fa89bf0256d19a9955fb0b417b9152
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-28 09:06:27 -0600

    Current Weather - developer comments added

--- src/component/CurrentWeather.js
+++ src/component/CurrentWeather.js
@@ -1,19 +1,35 @@
 import React from "react";
 
+/**
+ * Displays the current weather information
+ *
+ * This is a reusable component.
+ *
+ * @param {Object} props
+ */
 export default function CurrentWeather(props) {
+  // Object destructuring of currentForecast from props
   let { high, message } = props.currentForecast;
+
+  // This is for image rendering. Renders the image
+  // curresponding to the name of the "message"
   let imgMessage = "Sunny";
   if (message) {
     imgMessage = message;
   }
+
   return (
     <div className="city-latest-forecast">
+      {/* IMAGE */}
       <img
         src={require("../assets/" + imgMessage + ".png")}
         alt="Sunny Day"
         className="img-weather-title"
       />
+      {/* CURRENT TEMPERATURE (used highest for the exercise) */}
       <div className="current-temp">{high}&deg;</div>
+
+      {/* WEATHER DESCRIPTION */}
       <h4> {message}</h4>
     </div>
   );
