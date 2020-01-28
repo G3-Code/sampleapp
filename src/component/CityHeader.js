b16ff228b2ab237c5dc61985142d73a05601c4ba
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-28 09:03:19 -0600

    CityHeader -> Developer comments added

--- src/component/CityHeader.js
+++ src/component/CityHeader.js
@@ -1,11 +1,25 @@
 import React from "react";
 
+/**
+ * The header information in the forecast containing the city name and dropdown with cities.
+ *
+ * This is a reusable component.
+ *
+ * Note: Select handler looks to the default implementation in the parent currently. However,
+ * if need be it can also be implemented in the child class.
+ */
 export default class CityHeader extends React.Component {
+  // Render function
   render() {
+    // props object destructuring
     let { city, cities, handleSelect } = this.props;
+
     return (
       <div className="city-header">
+        {/* DISPLAYS THE CITY INFO TO LEFT */}
         <p>{city}</p>
+
+        {/* DISPLAYS THE DROPDOWN TO THE RIGHT */}
         <select
           ref="citySelector"
           onChange={() => handleSelect(this.refs.citySelector.value)}
